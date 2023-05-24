59c007881bd1331bbd2ecd2d786759a43d7b8359
Brady Hislop <39865746+hisl6802@users.noreply.github.com>
2023-05-24 15:37:04 -0600

    Add files via upload

---
+++ WindowsSetUp/Packages.bat
@@ -0,0 +1,15 @@
+cd C:\Users\Public\Documents\ClusteringGUI-develop
+
+pip install numpy
+pip install scipy
+pip install openpyxl
+pip install matplotlib
+pip install scikit-learn
+pip install pandas
+pip install fpdf
+pip install xlrd
+pip install seaborn
+pip install mplcursors
+pip install Bio
+pip install pingouin
+
